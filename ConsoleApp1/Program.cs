7f65740a1e3827097509746a1b915e63f0ec1641
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-06-21 20:27:28 -0500

    Example ASYNC

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,35 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
 
-            var obras = await GetObrasAsync();
-            foreach (var item in obras)
-            {
-                Console.WriteLine(item.codigoUnico);
-            }
+            //var obras = await GetObrasAsync();
+            //Task.Run(() => ShowObrasAsync());
+
+            //WriteLineAsync("primero",3000);
+            //WriteLineAsync("segundo",2000);
+            //WriteLineAsync("tercero",1000);
+
+
+            //Task.Run(() => WriteLineAsync("primero", 3000));
+            //Task.Run(() => WriteLineAsync("segundo", 2000));
+            //Task.Run(() => WriteLineAsync("tercero", 1000));
+
+            Task.Run(()=>  PostInsertAppoitmentAsync());
+
             Console.WriteLine("Hugo Torrico");
             Console.Read();
         }
@@ -43,6 +54,50 @@ namespace ConsoleApp1
             return obras;
         }
 
+
+        private static async Task PostInsertAppoitmentAsync()
+        {
+            HttpClient client = new HttpClient();
+            var urlBase = "http://18.232.226.120/WICAPIDev";
+            client.BaseAddress = new Uri(urlBase);
+            var url = string.Concat(urlBase, "/api/UserClass/InsertAppoitment");
+
+
+            var model = new AppoitmentRequest
+            {
+                appointmentId = 7,
+                appointmentNumber = "number",
+                appointmentURL = "URL",
+                classCode = "code",
+                //wicId = "1123132",
+                wicId = "235",
+                registeredAppointmentDateTime = DateTime.Today
+            };
+            //Qué te voy a enviar
+            var request = JsonConvert.SerializeObject(model);            
+            //Cómo te lo voy a enviar
+            var content = new StringContent(request, Encoding.UTF8, "application/json");
+           
+            var response = client.PostAsync(url, content).Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+
+                var result = await response.Content.ReadAsStringAsync();
+                var appoitment = JsonConvert.DeserializeObject<AppoitmentResponse>(result);
+                Console.WriteLine(appoitment.message);
+                Console.WriteLine(appoitment.messageEN);
+            }
+            else
+            {
+                Console.WriteLine("Error Del Servicio");
+            }
+
+
+
+        }
+
+
         private static async Task ShowObrasAsync()
         {
 
@@ -50,7 +105,7 @@ namespace ConsoleApp1
             var obras = new List<Obras>();
             var urlBase = "http://sitra.emape.gob.pe:8082";
             client.BaseAddress = new Uri(urlBase);
-            var url = string.Concat(urlBase, "/api/expedientes/obtener_proyectos/0");
+            var url = string.Concat(urlBase, "/api/expedientes/obtener_proyectos/2022");
 
             var response = client.GetAsync(url).Result;
 
@@ -63,9 +118,29 @@ namespace ConsoleApp1
             foreach (var item in obras)
             {
                 Console.WriteLine(item.codigoUnico);
+                break;
             }
-
+           
         }
+
+        static async void WriteLineAsync(string message,int miliseconds)
+        {
+            StringBuilder stringToWrite = new StringBuilder(message);
+            stringToWrite.AppendLine();
+            Thread.Sleep(miliseconds);
+            using (StringWriter writer = new StringWriter(stringToWrite))
+            {
+                UnicodeEncoding ue = new UnicodeEncoding();
+                char[] charsToAdd = ue.GetChars(ue.GetBytes(""));
+                foreach (char c in charsToAdd)
+                {
+                    await writer.WriteLineAsync(c);
+                }
+                Console.WriteLine(stringToWrite.ToString());
+            }
+        }
+
+
 
     }
 }
